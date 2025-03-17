48da44a0daed77cf69e9ddd23d3d6c1f1611cc56
yangwood <yangwoodll@gmail.com>
2025-03-17 17:47:56 +0800

    featrue: add request and response model

--- src/modelStruct/room.go
+++ src/modelStruct/room.go
@@ -1,6 +1,10 @@
 package modelStruct
 
-type LeaveParams struct {
+type EmptyParams struct {
+}
+
+type ToggleParams struct {
+	Status int `json:"status"`
 }
 
 type JoinParams struct {
@@ -19,4 +23,29 @@ type UserModel struct {
 	Status             int    `json:"status"`
 	HasAudioPermission bool   `json:"hasAudioPermission"`
 	HasVideoPermission bool   `json:"hasVideoPermission"`
+}
+
+type CustomDataParams struct {
+	Key   string          `json:"key"`
+	Value CustomDataValue `json:"value"`
+}
+
+type CustomDataValue struct {
+	Type   int            `json:"type"`
+	User   CustomDataUser `json:"user"`
+	UserID string         `json:"userID"`
+}
+
+type CustomDataUser struct {
+	ID            string `json:"id"`
+	Role          int    `json:"role"`
+	Name          string `json:"name"`
+	Avatar        string `json:"avatar"`
+	RoomID        string `json:"roomID"`
+	Status        int    `json:"status"`
+	VideoOn       bool   `json:"videoOn"`
+	AudioOn       bool   `json:"audioOn"`
+	ScreenVideoOn bool   `json:"screenVideoOn"`
+	ScreenAudioOn bool   `json:"screenAudioOn"`
+	OnStage       bool   `json:"onStage"`
 }
