55372fc2d703a72cffedcdb982d58986eccb0218
yangwood <yangwoodll@gmail.com>
2025-03-18 16:32:12 +0800

    fix: id int64

--- src/modelStruct/request.go
+++ src/modelStruct/request.go
@@ -8,7 +8,7 @@ type RequestModel struct {
 	Reply   string          `json:"reply"`
 	Forward string          `json:"forward"`
 	Params  json.RawMessage `json:"params"` // 使用指针以便处理 JsonNode
-	ID      int             `json:"id"`
+	ID      int64           `json:"id"`
 	Token   string          `json:"token"`
 	To      string          `json:"to"`
 	BaseRequest
