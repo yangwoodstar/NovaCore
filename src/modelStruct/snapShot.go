9b6eaa2236b5484ac6f96392d34331b341322356
yangwood <yangwoodll@gmail.com>
2025-03-24 21:48:47 +0800

    featrue: add volc snapshaot model

--- src/modelStruct/snapShot.go
+++ src/modelStruct/snapShot.go
@@ -1,10 +1,11 @@
 package modelStruct
 
 type SnapShotStart struct {
-	AppID  string `json:"AppId"`  // 应用的唯一标志
-	RoomID string `json:"RoomId"` // 房间 ID，是房间的唯一标志
-	TaskID string `json:"TaskId"` // 截图任务 ID
-	UserID string `json:"UserId"` // 用户 ID
+	AppID    string `json:"AppId"`    // 应用的唯一标志
+	RoomID   string `json:"RoomId"`   // 房间 ID，是房间的唯一标志
+	TaskID   string `json:"TaskId"`   // 截图任务 ID
+	UserID   string `json:"UserId"`   // 用户 ID
+	Interval int32  `json:"Interval"` // 截图间隔，单位为秒
 }
 
 type SnapShotStop struct {
