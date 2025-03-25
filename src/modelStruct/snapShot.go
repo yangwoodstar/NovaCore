40b61df077556732bd02e728bc756f99bbac542b
yangwood <yangwoodll@gmail.com>
2025-03-25 10:19:20 +0800

    fix: add callback model

--- src/modelStruct/snapShot.go
+++ src/modelStruct/snapShot.go
@@ -13,3 +13,13 @@ type SnapShotStop struct {
 	RoomID string `json:"RoomId"` // 房间 ID，是房间的唯一标志
 	TaskID string `json:"TaskId"` // 截图任务 ID
 }
+
+type SnapShotCallback struct {
+	AppID    string `json:"AppId"`    // 应用的唯一标志
+	RoomID   string `json:"RoomId"`   // 房间 ID，是房间的唯一标志
+	TaskID   string `json:"TaskId"`   // 截图任务 ID
+	ImageUrl string `json:"ImageUrl"` // 截图地址
+	Width    int32  `json:"Width"`    // 截图宽度
+	Height   int32  `json:"Height"`   // 截图高度
+	Size     int64  `json:"Size"`     // 文件大小
+}
