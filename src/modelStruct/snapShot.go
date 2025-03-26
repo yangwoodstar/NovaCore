4a435e6f7df501a175fc571b01e0759acff2ea71
yangwood <yangwoodll@gmail.com>
2025-03-26 14:28:31 +0800

    update ai snap shot

--- src/modelStruct/snapShot.go
+++ src/modelStruct/snapShot.go
@@ -1,11 +1,12 @@
 package modelStruct
 
 type SnapShotStart struct {
-	AppID    string `json:"AppId"`    // 应用的唯一标志
-	RoomID   string `json:"RoomId"`   // 房间 ID，是房间的唯一标志
-	TaskID   string `json:"TaskId"`   // 截图任务 ID
-	UserID   string `json:"UserId"`   // 用户 ID
-	Interval int32  `json:"Interval"` // 截图间隔，单位为秒
+	AppID       string `json:"AppId"`       // 应用的唯一标志
+	RoomID      string `json:"RoomId"`      // 房间 ID，是房间的唯一标志
+	TaskID      string `json:"TaskId"`      // 截图任务 ID
+	UserID      string `json:"UserId"`      // 用户 ID
+	Interval    int32  `json:"Interval"`    // 截图间隔，单位为秒
+	CallbackUrl string `json:"CallbackUrl"` // 截图回调地址
 }
 
 type SnapShotStop struct {
