1e119c482060ece47c57a1458a9dfb22b24dbcbd
yangwood <yangwoodll@gmail.com>
2025-03-31 14:57:49 +0800

    fix: oss path

--- src/modelStruct/transcodeMessage.go
+++ src/modelStruct/transcodeMessage.go
@@ -1,5 +1,13 @@
 package modelStruct
 
+// "year":"","grade":"","subject":"","term":""
+type OssPath struct {
+	Year    string `json:"year"`
+	Grade   string `json:"grade"`
+	Subject string `json:"subject"`
+	Term    string `json:"term"`
+}
+
 type TranscodeInfo struct {
 	ParentRoomID     string   `json:"parentRoomID"`
 	AppID            string   `json:"appID"`
@@ -11,4 +19,5 @@ type TranscodeInfo struct {
 	ProcessType      int      `json:"processType"`
 	DisasterRecovery bool     `json:"disasterRecovery"`
 	Priority         int      `json:"priority"`
+	OssPath          OssPath  `json:"ossPath"`
 }
