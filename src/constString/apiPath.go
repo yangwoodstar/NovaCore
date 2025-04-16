50082a4cb0d87ca7be8216931f154366bb7552d1
yangwood <yangwoodll@gmail.com>
2025-04-16 17:55:45 +0800

    fix: add roomInfo

--- src/constString/apiPath.go
+++ src/constString/apiPath.go
@@ -7,6 +7,7 @@ const (
 	LiveBackendCreatePlaybackPath = "/api/play-back/create"
 	LiveBackendUpdatePlaybackPath = "/api/video-source/update"
 	LiveReportTranscodePath       = "/api/report/playbackConverter"
+	LiveBackendGetRoomList        = "/api/room/status-list"
 )
 
 const (
