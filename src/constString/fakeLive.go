6bcdf06ef84cf5bf15cc7def78e74517b85aaa20
yangwood <yangwoodll@gmail.com>
2025-04-27 21:04:03 +0800

    featrue: add fake live signal send

--- src/constString/fakeLive.go
+++ src/constString/fakeLive.go
@@ -1,9 +1,11 @@
 package constString
 
 const (
-	FakeLiveStop    = 0
-	FakeLiveStart   = 1
-	FakeLiveRestart = 2
-	FakeLiveKill    = 3
-	FakeLiveToRtc   = 4
+	FakeLiveStop      = 0
+	FakeLiveStart     = 1
+	FakeLiveRestart   = 2
+	FakeLiveKill      = 3
+	FakeLiveToRtc     = 4
+	FakeLiveSendStart = 5
+	FakeLiveSendStop  = 6
 )
