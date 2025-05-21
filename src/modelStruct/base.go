3b64188d87088f5251c0b8217e1ae04a6196ef75
yangwood <yangwoodll@gmail.com>
2025-05-21 10:39:50 +0800

    featrue: add base room info

--- src/modelStruct/base.go
+++ src/modelStruct/base.go
@@ -1,11 +1,12 @@
 package modelStruct
 
 type BaseRoomInfo struct {
-	AppID  string `json:"appID"`
-	RoomID string `json:"roomID"`
-	UserID string `json:"userID"`
-	Reply  string `json:"reply"`
-	ID     int64  `json:"ID"`
-	Method string `json:"method"`
-	Role   int    `json:"role"`
+	AppID     string `json:"appID"`
+	RoomID    string `json:"roomID"`
+	UserID    string `json:"userID"`
+	Reply     string `json:"reply"`
+	ID        int64  `json:"ID"`
+	Method    string `json:"method"`
+	Role      int    `json:"role"`
+	SessionID string `json:"sessionID"`
 }
