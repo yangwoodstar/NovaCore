d55f5c052ba9312aae4da9ac5ff81019119e8186
yangwood <yangwoodll@gmail.com>
2025-06-27 20:33:45 +0800

    featrue: add rabbitmq url

---
+++ src/tools/rabbitmqApi.go
@@ -0,0 +1,11 @@
+package tools
+
+import "fmt"
+
+func GetExchangeBindingQueueUrl(domain, port, exchange, vhost string) string {
+	return fmt.Sprintf("http://%s:%s/api/exchanges/%s/%s/bindings/source", domain, port, vhost, exchange)
+}
+
+func GetQueueInfoUrl(domain, port, queue, vhost string) string {
+	return fmt.Sprintf("http://%s:%s/api/queues/%s/%s", domain, port, vhost, queue)
+}
