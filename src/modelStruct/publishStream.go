d240d5b5e554163849de57115809e68eff9ca0d2
yangwood <yangwoodll@gmail.com>
2025-07-16 11:15:07 +0800

    featrue: add publish ext

--- src/modelStruct/publishStream.go
+++ src/modelStruct/publishStream.go
@@ -4,13 +4,30 @@ type PublishStreamParams struct {
 	Stream PublishStream `json:"stream"`
 }
 
+type Position struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type TrimProfile struct {
+	Name     string   `json:"name"`
+	Position Position `json:"position"`
+	Width    int      `json:"width"`
+	Height   int      `json:"height"`
+}
+
+type PublishStreamExt struct {
+	TrimProfile []TrimProfile `json:"trimProfile"`
+}
+
 type PublishStream struct {
-	StreamID  string `json:"streamID"`
-	MediaType int    `json:"mediaType"`
-	Video     bool   `json:"video"`
-	Audio     bool   `json:"audio"`
-	Width     int    `json:"width"`
-	Height    int    `json:"height"`
+	StreamID  string            `json:"streamID"`
+	MediaType int               `json:"mediaType"`
+	Video     bool              `json:"video"`
+	Audio     bool              `json:"audio"`
+	Width     int               `json:"width"`
+	Height    int               `json:"height"`
+	Ext       *PublishStreamExt `json:"ext"` // 可选字段，可能为nil
 }
 
 type PublishStreamResponse struct {
