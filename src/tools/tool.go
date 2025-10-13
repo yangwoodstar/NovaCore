831ad113e2424ee7c41bf0941c8fdaa3a162af72
yangwood <yangwoodll@gmail.com>
2025-10-13 16:49:05 +0800

    featrue: add tc callback

--- src/tools/tool.go
+++ src/tools/tool.go
@@ -10,10 +10,12 @@ type RecordConfig struct {
 	AppID        string
 	RoomID       string
 	TaskID       string
+	TcTaskID     string
 	RecordType   int
 	FirstPrefix  string
 	SecondPrefix string
 	EnvType      string
+	ObjFileName  string
 }
 
 func CleanString(origin string) string {
@@ -39,4 +41,18 @@ func GetRecordFilePath(config *RecordConfig) string {
 	roomIDProcess := CleanString(config.RoomID)
 	fileName := fmt.Sprintf("%s_%s_%s.%s", config.AppID, config.RoomID, config.TaskID, fileType)
 	return fmt.Sprintf("/%s/%s/%s/%s/%s/%s", config.FirstPrefix, config.SecondPrefix, config.EnvType, appIDProcess, roomIDProcess, fileName)
+}
+
+func GetTcRecordFilePath(config *RecordConfig) string {
+	var fileType string
+	if config.RecordType == constString.AVType {
+		fileType = constString.MP4Suffix
+	} else if config.RecordType == constString.AudioType {
+		fileType = constString.MP3Suffix
+	} else if config.RecordType == constString.VideoType {
+		fileType = constString.MP4Suffix
+	}
+	withoutSuffix := strings.TrimSuffix(config.ObjFileName, ".m3u8")
+	fileName := fmt.Sprintf("%s.%s", withoutSuffix, fileType)
+	return fmt.Sprintf("/%s/%s/%s/%s/%s/%s/%s/%s/%s", "origin", config.FirstPrefix, config.SecondPrefix, config.EnvType, config.AppID, config.RoomID, config.TaskID, config.TcTaskID, fileName)
 }
