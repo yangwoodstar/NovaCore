d646d4c95a7fdd207148d9cf4e0022c2018eacaa
yangwood <yangwoodll@gmail.com>
2025-10-13 17:08:49 +0800

    fix: tx callback

--- src/modelStruct/tcCallback.go
+++ src/modelStruct/tcCallback.go
@@ -1,14 +1,29 @@
 package modelStruct
 
-type Payload struct {
-	Status int `json:"Status"`
+// FileMessage 结构体
+type FileMessage struct {
+	FileName       string `json:"FileName"`
+	UserId         string `json:"UserId"`
+	TrackType      string `json:"TrackType"`
+	MediaId        string `json:"MediaId"`
+	StartTimeStamp int64  `json:"StartTimeStamp"`
+	EndTimeStamp   int64  `json:"EndTimeStamp"`
 }
+
+// Payload 结构体
+type Payload struct {
+	Status      int           `json:"Status"`
+	FileList    []string      `json:"FileList"`
+	FileMessage []FileMessage `json:"FileMessage"`
+}
+
 type EventInfo struct {
-	RoomId    string `json:"RoomId"`
-	EventTs   string `json:"EventTs"`
-	EventMsTs int64  `json:"EventMsTs"`
-	UserId    string `json:"UserId"`
-	TaskId    string `json:"TaskId"`
+	RoomId    string  `json:"RoomId"`
+	EventTs   string  `json:"EventTs"`
+	EventMsTs int64   `json:"EventMsTs"`
+	UserId    string  `json:"UserId"`
+	TaskId    string  `json:"TaskId"`
+	Payload   Payload `json:"Payload"`
 }
 
 type EventCallback struct {
