50f7ab8de03d5c8b09a148c472b1d59e07b57801
yangwood <yangwoodll@gmail.com>
2025-11-06 20:06:56 +0800

    fix: update task pool model

--- src/taskPool/model.go
+++ src/taskPool/model.go
@@ -3,11 +3,14 @@ package taskPool
 import "github.com/yangwoodstar/NovaCore/src/transportCore"
 
 type Task struct {
-	RoomID string
-	AppID  string
-	Data   transportCore.UnificationMessage
-	Method string
-	Reply  string
-	Topic  string
-	Index  int
+	RoomID        string
+	AppID         string
+	Data          transportCore.UnificationMessage
+	Method        string
+	Reply         string
+	Topic         string
+	Index         int
+	Uuid          string
+	ParentRoomID  string
+	CurrentRoomID string
 }
