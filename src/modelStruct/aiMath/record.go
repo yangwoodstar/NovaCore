776b932ce626d65c504c0dede7ff99f59c10d938
yangwood <yangwoodll@gmail.com>
2025-11-17 11:43:56 +0800

    fix: add record query response struct

--- src/modelStruct/aiMath/record.go
+++ src/modelStruct/aiMath/record.go
@@ -19,3 +19,17 @@ type Response struct {
 	ResponseMetadata ResponseMetadata `json:"ResponseMetadata"`
 	Result           string           `json:"Result"`
 }
+
+type RecordQueryResponse struct {
+	Result Result `json:"Result"`
+}
+
+type Result struct {
+	RecordTask RecordTask `json:"RecordTask"`
+}
+
+type RecordTask struct {
+	StartTime int64 `json:"StartTime"` // 录制开始时间戳 (毫秒)
+	EndTime   int64 `json:"EndTime"`   // 录制结束时间戳 (毫秒)
+	Status    int   `json:"Status"`    // 任务状态
+}
