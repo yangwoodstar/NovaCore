50a956d5ef43c3cce608d3e6337e1b0673fd333b
yangwood <yangwoodll@gmail.com>
2025-11-27 17:54:16 +0800

    fix: add get rabbitmq queue info

--- src/tools/rabbitmqApi.go
+++ src/tools/rabbitmqApi.go
@@ -9,3 +9,7 @@ func GetExchangeBindingQueueUrl(domain, port, exchange, vhost string) string {
 func GetQueueInfoUrl(domain, port, queue, vhost string) string {
 	return fmt.Sprintf("http://%s:%s/api/queues/%s/%s", domain, port, vhost, queue)
 }
+
+func GetAllQueuesUrl(domain, port, vhost string) string {
+	return fmt.Sprintf("http://%s:%s/api/queues", domain, port, vhost)
+}
